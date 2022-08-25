9521f27c0b405537079d24d4969ad8ebc7abcaa7
Pawan Kumar <87439769+pawankr1994@users.noreply.github.com>
2022-08-25 15:19:24 +0530

    Delete API_Project directory

--- API_Project/Task_about _API.py
+++
@@ -1,6 +0,0 @@
-# 1. write a program to insert a record in sql table via API database.
-
-import mysql.connector as conn
-
-mydb = conn.connect(host = "localhost", user = "root", password = "Pawan@54463")
-cursor = mydb.cursor()
